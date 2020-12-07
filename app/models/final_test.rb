c56042fd049f5c4241427455403faa71bda4b7dc
Margaux Stanislas <margaux.stanislas@hotmail.fr>
2020-12-07 15:14:36 +0100

    update FinalTest model

--- app/models/final_test.rb
+++ app/models/final_test.rb
@@ -1,3 +1,4 @@
 class FinalTest < ApplicationRecord
   belongs_to :program
+  has_one_attached :photo
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_130856) do
+ActiveRecord::Schema.define(version: 2020_12_07_132014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2020_12_06_130856) do
     t.bigint "program_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
+    t.text "objectifs"
     t.index ["program_id"], name: "index_final_tests_on_program_id"
   end
 
