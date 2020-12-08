6b0fa435997a859caa1d0db9c4059b8dd5e6bfa2
Margaux Stanislas <margaux.stanislas@hotmail.fr>
2020-12-08 11:01:49 +0100

    creation relation usersprogram lors du commencement d'un program

--- app/controllers/users_programs_controller.rb
+++ app/controllers/users_programs_controller.rb
@@ -1,8 +1,11 @@
 class UsersProgramsController < ApplicationController
   def create
-
-  end
-
-  def set_params
+    up = UsersProgram.new(status: "in progress")
+    up.user = current_user
+    program = Program.find(params[:program_id])
+    up.program = program
+    if up.save
+      redirect_to program_path(program)
+    end
   end
 end
