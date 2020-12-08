af74827fa032641a20aff31c5da27aed05eeb577
clement piot <c.piot@me.com>
2020-12-08 10:30:22 +0100

    morning changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,27 @@ class User < ApplicationRecord
   has_many :users_programs, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
-  has_many :final_test_answers, dependent: :destroy
-  has_many :quizz_scores, dependent: :destroy
+
+  def global_stats
+    ((self.level.to_f * 1000).to_f + self.xp).to_f / 100.to_f
+  end
+
+  def count_inprogress
+    @nb_inprogress = UsersProgram.where(user: self, status: "in progress")
+    @nb_inprogress.count
+  end
+
+  def count_done
+    @nb_done = UsersProgram.where(user: self, status: "done")
+    @nb_done.count
+  end
+
+  def consecutive_days
+      if (self.last_sign_in_at).to_i == (self.current_sign_in_at).to_i - 1
+        count_days += 1
+      else
+        count_days = 0
+      end
+      return count_days
+  end
 end
