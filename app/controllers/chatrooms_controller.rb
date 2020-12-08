eb0fd5ddfe57a85d58f364ad45fe1af0cb38e23a
Margaux Stanislas <margaux.stanislas@hotmail.fr>
2020-12-08 17:33:20 +0100

    config channel

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,23 +1,10 @@
 class ChatroomsController < ApplicationController
   def create
-    @chatroom = Chatroom.find(params[:chatroom_id])
-    @message = Message.new(message_params)
-    @message.chatroom = @chatroom
-    @message.user = current_user
-    raise
-    if @message.save
-      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
-    else
-      render "chatrooms/show"
-    end
+
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
-  end
-
-  def message_params
-    params.require(:message).permit(:content)
   end
 end
